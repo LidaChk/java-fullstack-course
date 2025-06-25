5854681a52290f3487a814e17d4948a6463e78e9
LidaChk <lvglazko@gmail.com>
2025-06-25 09:08:59 +0300

    test: init  CustomListTest

--- src/test/java/org/example/week01/CustomListTest.java
+++ src/test/java/org/example/week01/CustomListTest.java
@@ -1,4 +1,45 @@
 package org.example.week01;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+
+
 public class CustomListTest {
+
+    @Nested
+    @DisplayName("Size and Empty operations")
+    class SizeAndEmptyTests {
+
+    }
+
+    @Nested
+    @DisplayName("Add operations")
+    class AddTests {
+
+    }
+
+    @Nested
+    @DisplayName("Get operations")
+    class GetTests {
+
+    }
+
+    @Nested
+    @DisplayName("Set operations")
+    class SetTests {
+
+    }
+
+    @Nested
+    @DisplayName("Remove operations")
+    class RemoveTests {
+
+    }
+
+    @Nested
+    @DisplayName("Clear operations")
+    class ClearTests {
+
+    }
 }
